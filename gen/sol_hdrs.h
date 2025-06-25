60f85aa85319ee18195d6c9da6283c63f519b981
David Rubin <david@vortan.dev>
2025-06-25 02:31:12 -0700

    update boringssl and bring it in as a depedency

--- gen/sol_hdrs.h
+++
@@ -1,5 +0,0 @@
-// header file for convienence of generating the translate_c.zig file
-// zig translate-c gen/sol_hdrs.h -I vendor/include
-
-#include <openssl/ssl.h>
-#include <openssl/curve25519.h>
